f58f23196fb61a36847b793eacdfffeb99f88451
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-02-21 22:58:16 +0700

    Add operate() function

--- script.js
+++ script.js
@@ -14,6 +14,23 @@ function multiply(a, b) {
     return a / b;
 }
 
+function operate(leftOperand, rightOperand, operator) {
+    switch (operator) {
+        case "+": 
+            return add(leftOperand, rightOperand);
+            break;
+        case "-":
+            return subtract(leftOperand, rightOperand);
+            break;
+        case "*":
+            return multiply(leftOperand, rightOperand);
+            break;
+        case "/":
+            return divide(leftOperand, rightOperand);
+    }
+}
+
 let leftOperand;
 let rightOperand;
 let operator;
+
